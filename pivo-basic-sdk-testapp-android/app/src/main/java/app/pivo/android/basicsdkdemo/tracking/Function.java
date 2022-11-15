aa9c1cf1699f2e2583fc6f5802cf24b44cb615ff
az0422 <87636192+az0422@users.noreply.github.com>
2022-11-15 20:07:30 +0900

    20221115r0
    새 가중치 적용
    SORT 알고리즘 적용 중

---
+++ pivo-basic-sdk-testapp-android/app/src/main/java/app/pivo/android/basicsdkdemo/tracking/Function.java
@@ -0,0 +1,13 @@
+package app.pivo.android.basicsdkdemo.tracking;
+
+public class Function{
+
+    public Matrix next( Matrix x, double time){
+        return Matrix.zero(1,1);
+    }
+
+    public Matrix jacobian(Matrix x, double time){
+        return Matrix.zero(1,1);
+    }
+
+}
