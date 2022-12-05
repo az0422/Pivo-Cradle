41502c87740cd162e8c5e51b17cad736d12b7f00
az0422 <87636192+az0422@users.noreply.github.com>
2022-12-05 18:37:44 +0900

    20221205r0
    새 가중치 적용

--- pivo-basic-sdk-testapp-android/app/src/main/java/app/pivo/android/basicsdkdemo/DetectorActivity.java
+++ pivo-basic-sdk-testapp-android/app/src/main/java/app/pivo/android/basicsdkdemo/DetectorActivity.java
@@ -383,7 +383,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
 
             case R.id.model_speed:
                 model_name = "speed";
-                modelSelect = "yolov5s.tflite";
+                modelSelect = "yolov7-lite.tflite";
                 output_shape = new int[]{ 10647, 10647 };
                 input_size = 416;
                 is_tiny = true;
